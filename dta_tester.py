0ad4eda30354d82e27ba808a2314e2b3151dd373
William Alexander <williamericalexander@gmail.com>
2019-12-06 22:41:30 -0600

    fixed string manipulation bug

--- dta_tester.py
+++ dta_tester.py
@@ -2,7 +2,7 @@ from dta.dta_env import dta_env
 
 env = dta_env(interval=60*60*5)
 for i in range(30):
-    with open(""+i+".txt",'w') as outfile:
+    with open(""+str(i)+".txt",'w') as outfile:
         ni = 60//5
         env = dta_env(interval=60*5, numIntervals=ni)
         state = env.reset()
