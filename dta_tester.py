8a62b7d422191b117008ba7df0ed848558e26480
William Alexander <williamericalexander@gmail.com>
2019-12-07 08:08:52 -0600

    retry

--- dta_tester.py
+++ dta_tester.py
@@ -1,10 +1,11 @@
 from dta.dta_env import dta_env
 
-env = dta_env(interval=60*60*5)
+ni = 60//5
+env = dta_env(interval=60*5,numIntervals = ni)
 for i in range(30):
+    # env = dta_env(interval=60*5, numIntervals=ni)
     with open(""+str(i)+".txt",'w') as outfile:
-        ni = 60//5
-        env = dta_env(interval=60*5, numIntervals=ni)
+        
         state = env.reset()
         for _ in range(ni):
             _, rew, _ = env.step()
